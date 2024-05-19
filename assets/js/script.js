fa96fedfc8f2f27b906551c83c50f26fe204d00d
Mtduffey36 <michael.t.duffey@gmail.com>
2024-05-19 17:51:57 -0500

    added variables to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 const openWeatherUrl = 'https://api.openweathermap.org';
 const weatherAPI = 'f04be44694b5a8d0bf6454eaa0e1646b';
-const searchHistory = [];
+
+const input = document.querySelector('#input');
+const inputValue = document.querySelector('#inputValue');
+const submitBtn = document.querySelector('#submitBtn');
+
+const searchHistory = document.querySelector('#searchHistory');
+
+const cityName = document.querySelector('#cityName');
+const dayTime = document.querySelector('#dayTime');
+const dayWind = document.querySelector('#dayWind');
+const dayHumidity = document.querySelector('#dayHumidity');
+
+const forecast = document.querySelector('#forecast');
+
