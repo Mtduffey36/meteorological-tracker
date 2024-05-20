f0a9faeb293d2be277cab084e53e67fe97a817dd
Mtduffey36 <michael.t.duffey@gmail.com>
2024-05-19 20:41:16 -0500

    added JS for fetch API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,72 @@
 const openWeatherUrl = 'https://api.openweathermap.org';
 const weatherAPI = 'f04be44694b5a8d0bf6454eaa0e1646b';
 
-const input = document.querySelector('#input');
-const inputValue = document.querySelector('#inputValue');
-const submitBtn = document.querySelector('#submitBtn');
+const input = document.getElementById('input');
+const cityNameInput = document.getElementById('cityNameInput');
+const SubmitBtn = document.getElementById('submitBtn');
 
-const searchHistory = document.querySelector('#searchHistory');
+const searchHistory = document.getElementById('searchHistory');
 
-const cityName = document.querySelector('#cityName');
-const dayTime = document.querySelector('#dayTime');
-const dayWind = document.querySelector('#dayWind');
-const dayHumidity = document.querySelector('#dayHumidity');
+const cityName = document.getElementById('cityName');
+const dayTime = document.getElementById('dayTime');
+const dayWind = document.getElementById('dayWind');
+const dayHumidity = document.getElementById('dayHumidity');
 
-const forecast = document.querySelector('#forecast');
+const forecast = document.getElementById('forecast');
 
+// dayjs.extend(window.dayjs_plugin_utc);
+// dayjs.extend(window.dayjs_plugin_timezone);
+
+
+// function saveInput () {
+//     const cityList = JSON.parse(localStorage.getItem("cityList"));
+     
+//      const post = {
+//         inputValue: inputValue.value.trim(),
+//      };
+   
+//      if(cityList === null){
+//          localStorage.setItem("cityList", JSON.stringify([post])); 
+//      } else { 
+//          cityList.push(post);
+       
+//          localStorage.setItem("List", JSON.stringify(blogList));
+//      }; 
+//  };
+
+//   document.addEventListener('DOMContentLoaded', function() {
+//     document.querySelector('button').addEventListener('#submitBtn', function(event) {
+//         event.preventDefault();
+//     })
+//   });
+
+//   function validateCity() {
+//     const city = document.getElementById('input').value.trim();
+
+//     if (city === "") {
+//         alert("Please Enter a City!");
+//         return false;
+//     };
+//     return true;
+//   };
+
+document.getElementById('submitBtn').addEventListener('click', function() {
+    console.log('here');
+    const coords = getLocationByCity(cityNameInput.value.trim());
+
+})
+
+async function getLocationByCity(cityName) {
+    const cityApi = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${weatherAPI}`
+    const response = await fetch(cityApi);
+    
+
+    const data = response.json();
+    console.log(data);
+    const coords = {
+        latitude: [0].lat,
+        longitude: [0].lon,
+    };
+    console.log(coords);
+
+}
