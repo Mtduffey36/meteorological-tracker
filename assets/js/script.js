8a7840329edea4863898529f6f2804a82350330c
Mtduffey36 <michael.t.duffey@gmail.com>
2024-05-26 22:27:56 -0500

    code cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,98 +1,7 @@
-// const openWeatherUrl = 'https://api.openweathermap.org';
-// const weatherAPI = 'f04be44694b5a8d0bf6454eaa0e1646b';
-
-// const input = document.getElementById('input');
-// const cityNameInput = document.getElementById('cityNameInput');
-// const SubmitBtn = document.getElementById('submitBtn');
-
-// const searchHistory = document.getElementById('searchHistory');
-
-// const cityName = document.getElementById('cityName');
-// const dayTime = document.getElementById('dayTime');
-// const dayWind = document.getElementById('dayWind');
-// const dayHumidity = document.getElementById('dayHumidity');
-
-// const forecast = document.getElementById('forecast');
-
-// // dayjs.extend(window.dayjs_plugin_utc);
-// // dayjs.extend(window.dayjs_plugin_timezone);
-
-
-// // function saveInput () {
-// //     const cityList = JSON.parse(localStorage.getItem("cityList"));
-     
-// //      const post = {
-// //         inputValue: inputValue.value.trim(),
-// //      };
-   
-// //      if(cityList === null){
-// //          localStorage.setItem("cityList", JSON.stringify([post])); 
-// //      } else { 
-// //          cityList.push(post);
-       
-// //          localStorage.setItem("List", JSON.stringify(blogList));
-// //      }; 
-// //  };
-
-// //   document.addEventListener('DOMContentLoaded', function() {
-// //     document.querySelector('button').addEventListener('#submitBtn', function(event) {
-// //         event.preventDefault();
-// //     })
-// //   });
-
-//   function validateCity() {
-//     const city = document.querySelector('input').value.trim();
-
-//     if (city === "") {
-//         alert("Please Enter a City!");
-//         return false;
-//     };
-//     return true;
-//   };
-
-// document.getElementById('submitBtn').addEventListener('click', function(event) {
-//     event.preventDefault();
-//     const forecast = getForecastByCity(cityNameInput.value.trim())
-//     const coords = getLocationByCity(cityNameInput.value.trim());
-
-// })
-
-// async function getLocationByCity(cityNameInput) {
-//     const cityApi = `http://api.openweathermap.org/geo/1.0/direct?q=${cityNameInput}&appid=${weatherAPI}`;
-//     const response = await fetch(cityApi);
-    
-
-//     const data = response.json();
-//     // console.log(data);
-//     const coords = {
-//         latitude: [0].lat,
-//         longitude: [0].lon,
-//     };
-
-// }
-
-// async function getForecastByCity(cityName) {
-//     const forecastAPI = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${weatherAPI}`;
-//     const fResponse = await fetch(forecastAPI);
-
-//     const fData = fResponse.json();
-//     console.log(fData);
-
-//     const forecast = {
-//         temp: fData.list[0].main.temp,
-//         humidity: fData.list[0].main.humidity,
-//     }
-
-// }
-
 const weatherApi = 'f04be44694b5a8d0bf6454eaa0e1646b' //this is my API key
 const cardsContainer = document.getElementById('card');
 const todayContainer = document.getElementById('current');
 const forecastHeader = document.getElementById('forecast');
-
-
-
-
 
 
 document.getElementById('submitBtn').addEventListener('click', function(event) {
@@ -109,8 +18,6 @@ document.getElementById('submitBtn').addEventListener('click', function(event) {
         alert('Please Enter City Name!');
     }
 })
-
-
 
 function addCityToHistory(city) {
     const historyList = document.getElementById('history-list');
@@ -145,7 +52,6 @@ function loadCities() {
 }
 
 document.addEventListener('DOMContentLoaded', loadCities);
-
 
 //Calls api information for the current weather
 function current(city) {
@@ -243,7 +149,6 @@ function forecastCard(forecastData) {
     forecastArea.append(dateEl);
 
     console.log(forecastCard);
-
 }
 
 
